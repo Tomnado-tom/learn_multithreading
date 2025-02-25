deabdef10158eef508d2cf00062293d2a341b106
Tom Dell <megamareep@gmail.com>
2025-02-25 17:00:04 +0000

    Add more threads. add latch to start them. Also Change to use Shared fence. Copy paste error

--- executable/source/main.cpp
+++ executable/source/main.cpp
@@ -2,13 +2,17 @@
 #include <library/library.h>
 #include <atomic>
 #include <thread>
+#include <latch>
 
 std::atomic_bool shared_flag;
 int shared_data = 0;
 
+std::latch work_done1{ 5 };
+std::latch work_done2{ 5 };
 
 void IncrementSharedRelaxedIssues()
 {
+    work_done1.arrive_and_wait();
     for (int count{ 0 }; count < 1000000;)
     {
         bool expected = false;
@@ -23,6 +27,7 @@ void IncrementSharedRelaxedIssues()
 
 void IncrementSharedFence()
 {
+    work_done2.arrive_and_wait();
     for (int count{ 0 }; count < 1000000;)
     {
         bool expected = false;
@@ -39,15 +44,22 @@ void IncrementSharedFence()
 int main()
 {
     {
+
         std::jthread thread1(&IncrementSharedRelaxedIssues);
         std::jthread thread2(&IncrementSharedRelaxedIssues);
+        std::jthread thread3(&IncrementSharedRelaxedIssues);
+        std::jthread thread4(&IncrementSharedRelaxedIssues);
+        std::jthread thread5(&IncrementSharedRelaxedIssues);
     }
 
     std::cout << "Shared data: " << shared_data << std::endl;
     shared_data = 0;
     {
-        std::jthread thread1(&IncrementSharedRelaxedIssues);
-        std::jthread thread2(&IncrementSharedRelaxedIssues);
+        std::jthread thread1(&IncrementSharedFence);
+        std::jthread thread2(&IncrementSharedFence);
+        std::jthread thread3(&IncrementSharedFence);
+        std::jthread thread4(&IncrementSharedFence);
+        std::jthread thread5(&IncrementSharedFence);
     }
 
     std::cout << "Shared data: " << shared_data << std::endl;
