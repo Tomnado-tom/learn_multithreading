890c9298fa1f26da13bd9f46717ba745b44c448f
Tom Dell <delltom89@gmail.com>
2025-02-25 15:47:07 +0000

    Add VSCode settings, update CMake version, and implement atomic operations in main.cpp

--- executable/source/main.cpp
+++ executable/source/main.cpp
@@ -1,11 +1,56 @@
 #include <iostream>
 #include <library/library.h>
+#include <atomic>
+#include <thread>
 
-int main() {
-    std::cout << "Hello, C++ Project!" << std::endl;
+std::atomic_bool shared_flag;
+int shared_data = 0;
 
-    // Example usage of the library function
-    library::testFunction();
+
+void IncrementSharedRelaxedIssues()
+{
+    for (int count{ 0 }; count < 1000000;)
+    {
+        bool expected = false;
+        if (shared_flag.compare_exchange_strong(expected, true, std::memory_order_relaxed))
+        {
+            shared_data++;
+            count++;
+            shared_flag.store(false, std::memory_order_relaxed);
+        }
+    }
+}
+
+void IncrementSharedFence()
+{
+    for (int count{ 0 }; count < 1000000;)
+    {
+        bool expected = false;
+        if (shared_flag.compare_exchange_strong(expected, true, std::memory_order_acquire))
+        {
+            shared_data++;
+            count++;
+            shared_flag.store(false, std::memory_order_release);
+        }
+    }
+}
+
+
+int main()
+{
+    {
+        std::jthread thread1(&IncrementSharedRelaxedIssues);
+        std::jthread thread2(&IncrementSharedRelaxedIssues);
+    }
+
+    std::cout << "Shared data: " << shared_data << std::endl;
+    shared_data = 0;
+    {
+        std::jthread thread1(&IncrementSharedRelaxedIssues);
+        std::jthread thread2(&IncrementSharedRelaxedIssues);
+    }
+
+    std::cout << "Shared data: " << shared_data << std::endl;
 
     return 0;
 }
