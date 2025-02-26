86a72cf1a0d45b5fbc4f84a15ba3177521c6d6a7
Tom Dell <delltom89@gmail.com>
2025-02-26 11:49:08 +0000

    Add new increment functions with relaxed and fence memory orders, update VSCode settings for additional C++ headers, and clean up main function calls

--- executable/source/main.cpp
+++ executable/source/main.cpp
@@ -11,6 +11,40 @@ uint64_t shared_data = 0;
 
 std::barrier barrier{ 5 };
 
+
+void IncrementSharedRelaxedIssuesWeak()
+{
+    barrier.arrive_and_wait();
+    for (int count{ 0 }; count < 1000000;)
+    {
+        bool expected = false;
+        //this doesn't work. Which is expected.
+        while (!shared_flag.compare_exchange_weak(expected, true, std::memory_order_relaxed, std::memory_order_relaxed) && !expected);
+
+        shared_data++;
+        --shared_data;
+        count++;
+        shared_flag.store(false, std::memory_order_relaxed);
+    }
+}
+
+void IncrementSharedFenceWeak()
+{
+    barrier.arrive_and_wait();
+    for (int count{ 0 }; count < 1000000;)
+    {
+        bool expected = false;
+        //this doesn't work but i'm not sure why.
+        //maybe only use compare_exchange_weak for single data swaps. e.g swapping head of a stack.
+        while (!shared_flag.compare_exchange_weak(expected, true, std::memory_order_acquire) && !expected);
+
+        shared_data++;
+        --shared_data;
+        count++;
+        shared_flag.store(false, std::memory_order_release);
+    }
+}
+
 void IncrementSharedRelaxedIssues()
 {
     barrier.arrive_and_wait();
@@ -43,17 +77,6 @@ void IncrementSharedFence()
     }
 }
 
-void IncrementSharedUnsafe()
-{
-    barrier.arrive_and_wait();
-    for (int count{ 0 }; count < 1000000;)
-    {
-        shared_data++;
-        --shared_data;
-        count++;
-    }
-}
-
 std::mutex shared_mutex;
 
 void IncrementSharedWithMutex()
@@ -72,12 +95,13 @@ template<typename fn>
 void RunThread(fn&& function)
 {
     std::chrono::high_resolution_clock::time_point start_time = std::chrono::high_resolution_clock::now();
-    std::jthread thread1(function);
-    std::jthread thread2(function);
-    std::jthread thread3(function);
-    std::jthread thread4(function);
-    std::jthread thread5(function);
-
+    {
+        std::jthread thread1(function);
+        std::jthread thread2(function);
+        std::jthread thread3(function);
+        std::jthread thread4(function);
+        std::jthread thread5(function);
+    }
     std::chrono::high_resolution_clock::time_point end_time = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed_time = end_time - start_time;
     std::cout << "Elapsed time: " << elapsed_time.count() << " seconds" << std::endl;
@@ -86,40 +110,30 @@ void RunThread(fn&& function)
 
 int main()
 {
+    std::cout << "IncrementSharedWithMutex: " << std::endl;
+    RunThread(&IncrementSharedWithMutex);
+    std::cout << "Shared data: " << shared_data << std::endl;
+    shared_data = 0;
+
+    std::cout << "IncrementSharedRelaxedIssues: " << std::endl;
     RunThread(&IncrementSharedRelaxedIssues);
-
-    std::cout << "Shared data: " << shared_data << std::endl;
-
-
-    shared_data = 0;
-    {
-        std::jthread thread1(&IncrementSharedFence);
-        std::jthread thread2(&IncrementSharedFence);
-        std::jthread thread3(&IncrementSharedFence);
-        std::jthread thread4(&IncrementSharedFence);
-        std::jthread thread5(&IncrementSharedFence);
-    }
-
-    std::cout << "Shared data: " << shared_data << std::endl;
-
-    shared_data = 0;
-    {
-        std::jthread thread1(&IncrementSharedWithMutex);
-        std::jthread thread2(&IncrementSharedWithMutex);
-        std::jthread thread3(&IncrementSharedWithMutex);
-        std::jthread thread4(&IncrementSharedWithMutex);
-        std::jthread thread5(&IncrementSharedWithMutex);
-    }
     std::cout << "Shared data: " << shared_data << std::endl;
     shared_data = 0;
-    {
-        std::jthread thread1(&IncrementSharedUnsafe);
-        std::jthread thread2(&IncrementSharedUnsafe);
-        std::jthread thread3(&IncrementSharedUnsafe);
-        std::jthread thread4(&IncrementSharedUnsafe);
-        std::jthread thread5(&IncrementSharedUnsafe);
-    }
+
+    std::cout << "IncrementSharedFence: " << std::endl;
+    RunThread(&IncrementSharedFence);
     std::cout << "Shared data: " << shared_data << std::endl;
+    shared_data = 0;
+
+    std::cout << "IncrementSharedRelaxedIssuesWeak: " << std::endl;
+    RunThread(&IncrementSharedRelaxedIssuesWeak);
+    std::cout << "Shared data: " << shared_data << std::endl;
+    shared_data = 0;
+
+    std::cout << "IncrementSharedFenceWeak: " << std::endl;
+    RunThread(&IncrementSharedFenceWeak);
+    std::cout << "Shared data: " << shared_data << std::endl;
+    shared_data = 0;
 
     return 0;
 }
