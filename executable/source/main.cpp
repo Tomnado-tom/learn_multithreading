a4842e7d2d3dfd0fb70b832eb587b3e9d359eb03
Tom Dell <delltom89@gmail.com>
2025-02-26 10:26:01 +0000

    Enhance threading model: add chrono support, enable tests in CMake, and refactor main to use RunThread function

--- executable/source/main.cpp
+++ executable/source/main.cpp
@@ -4,6 +4,7 @@
 #include <thread>
 #include <barrier>
 #include <mutex>
+#include <chrono>
 
 std::atomic_bool shared_flag;
 uint64_t shared_data = 0;
@@ -19,6 +20,7 @@ void IncrementSharedRelaxedIssues()
         if (shared_flag.compare_exchange_strong(expected, true, std::memory_order_relaxed))
         {
             shared_data++;
+            --shared_data;
             count++;
             shared_flag.store(false, std::memory_order_relaxed);
         }
@@ -34,6 +36,7 @@ void IncrementSharedFence()
         if (shared_flag.compare_exchange_strong(expected, true, std::memory_order_acquire))
         {
             shared_data++;
+            --shared_data;
             count++;
             shared_flag.store(false, std::memory_order_release);
         }
@@ -46,6 +49,7 @@ void IncrementSharedUnsafe()
     for (int count{ 0 }; count < 1000000;)
     {
         shared_data++;
+        --shared_data;
         count++;
     }
 }
@@ -59,22 +63,34 @@ void IncrementSharedWithMutex()
     {
         std::lock_guard<std::mutex> lock(shared_mutex);
         shared_data++;
+        --shared_data;
         count++;
     }
+}
+
+template<typename fn>
+void RunThread(fn&& function)
+{
+    std::chrono::high_resolution_clock::time_point start_time = std::chrono::high_resolution_clock::now();
+    std::jthread thread1(function);
+    std::jthread thread2(function);
+    std::jthread thread3(function);
+    std::jthread thread4(function);
+    std::jthread thread5(function);
+
+    std::chrono::high_resolution_clock::time_point end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_time = end_time - start_time;
+    std::cout << "Elapsed time: " << elapsed_time.count() << " seconds" << std::endl;
 }
 
 
 int main()
 {
-    {
-        std::jthread thread1(&IncrementSharedRelaxedIssues);
-        std::jthread thread2(&IncrementSharedRelaxedIssues);
-        std::jthread thread3(&IncrementSharedRelaxedIssues);
-        std::jthread thread4(&IncrementSharedRelaxedIssues);
-        std::jthread thread5(&IncrementSharedRelaxedIssues);
-    }
+    RunThread(&IncrementSharedRelaxedIssues);
 
     std::cout << "Shared data: " << shared_data << std::endl;
+
+
     shared_data = 0;
     {
         std::jthread thread1(&IncrementSharedFence);
